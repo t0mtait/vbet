b025d161a5ba2bc595bfc92be0b7c11e5707bc8a
tom tait <thomas.t.ca@outlook.com>
2025-04-08 16:29:34 -0300

    feat: added user balance reduction upon bet creation

--- vbet/app/page.tsx
+++ vbet/app/page.tsx
@@ -1,10 +1,13 @@
-import Navbar from './components/Navbar';
+import React from 'react';
+import N from './components/Navbar';
+import "../app/globals.css"
 import Options from './components/Options';  // Ensure correct path
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-4 py-24 bg-gray-900">
-      <Navbar/>
+      <N />
       <Options />
     </main>
   );
