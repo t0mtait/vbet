c9612a0da0c1e521c9102d42a2af6d9b2418f2ad
tom tait <thomas.t.ca@outlook.com>
2025-04-08 16:27:23 -0300

    fix: navbar now uses white text no matter what view you're in

--- vbet/app/components/Navbar.js
+++ vbet/app/components/Navbar.js
@@ -9,16 +9,16 @@ const NavbarComponent = () => {
       </NavbarBrand>
       <NavbarToggle />
       <NavbarCollapse>
-        <NavbarLink as={Link} href="/" className="text-white">
+        <NavbarLink as={Link} href="/" className="custom-navbar-link">
           Home
         </NavbarLink>
-        <NavbarLink as={Link} href="/leaderboard" className="text-white">
+        <NavbarLink as={Link} href="/leaderboard" className="custom-navbar-link">
           Leaderboard
         </NavbarLink>
-        <NavbarLink as={Link} href="/bets" className="text-white">
+        <NavbarLink as={Link} href="/bets" className="custom-navbar-link">
           Bets
         </NavbarLink>
-        <NavbarLink as={Link} href="/releases" className="text-white">
+        <NavbarLink as={Link} href="/releases" className="custom-navbar-link">
           Releases
         </NavbarLink>
       </NavbarCollapse>
