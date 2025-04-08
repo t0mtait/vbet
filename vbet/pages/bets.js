b08cb15c7c11227c3df60660446eadc622705d8d
tom tait <thomas.t.ca@outlook.com>
2025-04-08 16:30:25 -0300

    feat: added bet settler input field and buttons on bets view. no backend functionality yet.

--- vbet/pages/bets.js
+++ vbet/pages/bets.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Navbar from '../app/components/Navbar';
 import "../app/globals.css"
 import B from '../app/components/Bets'
+import Settler from '../app/components/betSettler'
 const Bets = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-4 py-24 bg-gray-900">
       <div>
           <Navbar />
+          <Settler/>
           <B />
+
       </div>
     </main>
   );
