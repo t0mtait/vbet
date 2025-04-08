6d6d6f60f5b85486a8cdeb6a4caba23be58d6bde
tom tait <thomas.t.ca@outlook.com>
2025-04-08 18:42:56 -0300

    fix: added sorting (by name) in options table

--- vbet/pages/api/getOptions.js
+++ vbet/pages/api/getOptions.js
@@ -8,7 +8,8 @@ export default async function handler(req, res) {
       TableName: 'vbet-options', 
     };
     const result = await dynamoDB.scan(params).promise();
-    res.status(200).json(result.Items); // Send the fetched items
+    const sortedItems = result.Items.sort((a, b) => b.name - a.name);
+    res.status(200).json(sortedItems); // Send the fetched items
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
