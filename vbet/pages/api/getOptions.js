6e061cdca10d7827a390589b573551ebf9cc7982
tom tait <thomas.t.ca@outlook.com>
2025-04-08 20:48:50 -0300

    fix: sort options in ascending order not descending

--- vbet/pages/api/getOptions.js
+++ vbet/pages/api/getOptions.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
       TableName: 'vbet-options', 
     };
     const result = await dynamoDB.scan(params).promise();
-    const sortedItems = result.Items.sort((a, b) => b.name.localeCompare(a.name)); // Sort alphabetically in descending order
+    const sortedItems = result.Items.sort((a, b) => a.name.localeCompare(b.name)); // Sort alphabetically in ascending order
     res.status(200).json(sortedItems); // Send the fetched items
   } catch (error) {
     res.status(500).json({ error: error.message });
